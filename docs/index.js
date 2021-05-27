80afa77a2bb0f10cf1f46da82827f5bb41d84417
Even Alander <ea@pexip.com>
2021-05-27 09:12:42 +0200

    unify request function, always include credentials to also set cookie

--- docs/index.js
+++ docs/index.js
@@ -1,19 +1,7 @@
-function login(code) {
+function request(action) {
+  const params = new URLSearchParams(action).toString();
   return fetch(
-    `https://7po1em59g7.execute-api.eu-central-1.amazonaws.com/default/instagram-gateway?action=login&code=${code}`
-  )
-    .then((res) => {
-      console.log(res);
-      return res;
-    })
-    .then((res) => res.json())
-    .then((data) => console.log(data))
-    .catch((reason) => console.error(reason));
-}
-
-function get(action) {
-  return fetch(
-    `https://7po1em59g7.execute-api.eu-central-1.amazonaws.com/default/instagram-gateway?action=${action}`,
+    `https://7po1em59g7.execute-api.eu-central-1.amazonaws.com/default/instagram-gateway?${params}`,
     { credentials: "include" }
   )
     .then((res) => {
@@ -30,8 +18,11 @@ function init() {
   const code = params.get("code");
 
   if (code) {
-    login(code);
+    login({ action: "login", code });
   }
 }
 
 init();
+
+const profile = () => request({ action: "list_profile" });
+const media = () => request({ action: "list_media" });
